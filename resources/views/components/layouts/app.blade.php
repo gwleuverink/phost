8e1238530ff05bbc0c87ee8126ab52c2376a62bf
Willem Leuverink <willem@leuver.ink>
2024-03-20 15:00:43 +0100

    setup livewire & tailwind

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'Page Title' }}</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        {{ $slot }}
+    </body>
+</html>
