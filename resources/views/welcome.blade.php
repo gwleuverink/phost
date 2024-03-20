60ea23da57bccb2a1243c4acdd8dc9d7af95e68e
Willem Leuverink <willem@leuver.ink>
2024-03-20 15:04:50 +0100

    bootstrap inbox component

--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layouts.app>
-
-    <h1 class="text-2xl">Hello World!</h1>
-
-</x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Livewire\Inbox::class);
