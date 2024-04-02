7071b68c9e5a6cc712661ca2688eca32a7e73c6f
Willem Leuverink <willem@leuver.ink>
2024-04-02 08:59:51 +0200

    wip

---
+++ resources/views/components/message-list-item.blade.php
@@ -0,0 +1,10 @@
+<a href="#" class="block py-5 border-b px-4 transition hover:bg-indigo-100">
+
+    <div class="flex justify-between items-center">
+        <h3 class="text-lg text-gray-800 font-medium">John Appleseed</h3>
+        <p class="text-md text-gray-400">23m ago</p>
+    </div>
+
+    <div class="text-md italic text-gray-400">You have been invited!</div>
+
+</a>
