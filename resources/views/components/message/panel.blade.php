181f537a11890200524904463bcf9317dbc99e59
Willem Leuverink <willem@leuver.ink>
2024-04-05 16:52:09 +0200

    boyscouting

--- resources/views/components/message/panel.blade.php
+++ resources/views/components/message/panel.blade.php
@@ -1,5 +1,7 @@
 <div x-tabs:panel>
-    <div {{ $attributes->class("mb-4 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-md") }}>
-        {{ $slot }}
+    <div class="mb-4 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-md" x-cloak>
+        <div {{ $attributes->class("overflow-x-auto") }}>
+            {{ $slot }}
+        </div>
     </div>
 </div>
